55ae990f01bb39ab22380229ae6fc582846b00ee
A-Cobble <alex.cobble42@gmail.com>
2023-03-16 11:51:54 -0700

    Made significant progress on the letter pyramid

--- Letter_Pyramid/main.cpp
+++ Letter_Pyramid/main.cpp
@@ -10,12 +10,20 @@ int main() {
 	getline(cin, input);
 
 	for (size_t i{ 0 }; i < input.size(); i++) {
-		cout << input[i] << endl;
-		for (size_t j{ i }; j < input.size() - 1; j++) {
-			cout << input[j];
+		//cout << input[i] << endl;
+		for (size_t j{ 0 }; j < input.size() - i; j++) {
+			cout << " ";
 		}
-	}
 
+		for (size_t x{0}; x < (input.size() + 1) - (input.size() - i); x++) {
+			cout << input[x];
+		}
+
+		for (size_t y{ 0 }; y < input.size() - (input.size() - i); y++) {
+			cout << input[y];
+		}
+		cout << endl;
+	}
 	cout << endl;
 	return 0;
 }
